2695b19398be25859138e521168f5f230e285da2
southsw3 <southsw3@naver.com>
2025-02-21 17:21:34 +0900

    study66

---
+++ study ReadLine/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace study_ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////사용자 입력을 문자열 받기
+            //Console.Write("이름을 입력하세요.");
+            //string userName = Console.ReadLine();
+
+            //Console.WriteLine($"안녕하세요, {userName}님!");
+
+            //문자열을 정수로 변환
+            //Console.Write("나이를 입력하세요");
+            //string input = Console.ReadLine();
+            //int age = int.Parse(input);
+
+            //Console.WriteLine($"내년에는 {age + 1}살이 되겠군요!");
+            //Console.WriteLine("내년에는" + age + "살이 되겠군요!");
+            //Console.WriteLine("내년에는 {0} 살이 되겠군요!", age);
+
+            //float ruin;
+            //float card;
+            //float awakening;
+            //int manaMax;
+            //int manaHF;
+            //int manaHNF;
+            //float 이동속도;
+            //float 탈것속도;
+            //float 운반속도;
+            //float Skill_CoolTime;
+
+            //string input;
+
+            //Console.Write("루인 스킬 피해(%)");
+            //input = Console.ReadLine(); ruin = float.Parse(input);
+            //Console.Write("카드 게이지 획득량(%)");
+            //input = Console.ReadLine(); card = float.Parse(input);
+            //Console.Write("각성기 피해(%)");
+            //input = Console.ReadLine(); awakening = float.Parse(input);
+            //Console.Write("최대 마나");
+            //input = Console.ReadLine(); manaMax = int.Parse(input);
+            //Console.Write("전투 중 마나 회복량");
+            //input = Console.ReadLine(); manaHF = int.Parse(input);
+            //Console.Write("비전투 중 마나 회복량");
+            //input = Console.ReadLine(); manaHNF = int.Parse(input);
+            //Console.Write("이동 속도(%)");
+            //input = Console.ReadLine(); 이동속도 = float.Parse(input);
+            //Console.Write("탈것 속도(%)");
+            //input = Console.ReadLine(); 탈것속도 = float.Parse(input);
+            //Console.Write("운반 속도(%)");
+            //input = Console.ReadLine(); 운반속도 = float.Parse(input);
+            //Console.Write("스킬 재사용 대기시간 감소(%)");
+            //input = Console.ReadLine(); Skill_CoolTime = float.Parse(input);
+
+            //로딩바 시작화면
+            //게임스토리1
+            //비주얼 노벨 만들어보기
+
+            Console.WriteLine("Loading.");
+            Console.WriteLine("□□□□□");
+            Thread.Sleep(1000); //1000 1초
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("Loading..");
+            Console.WriteLine("■□□□□");
+            Thread.Sleep(1000); //1000 1초
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("Loading...");
+            Console.WriteLine("■■□□□");
+            Thread.Sleep(1000); //1000 1초
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("Loading.");
+            Console.WriteLine("■■■□□");
+            Thread.Sleep(1000); //1000 1초
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("Loading..");
+            Console.WriteLine("■■■■□");
+            Thread.Sleep(1000); //1000 1초
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("Loading...");
+            Console.WriteLine("■■■■■");
+            Console.Clear();
+
+            Console.WriteLine("엔터를 치면 스토리가 시작됩니다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신은 전쟁에 참여해야 합니다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신의 보직을 선택하여 주십시오. 1) 기관총 사수  2) 포병");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("맞서 싸우시겠습니까? 도망가시겠습니까? 1) 전쟁에 참여한다.   2) 도망간다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("최전방으로 가시겠습니까? 1) 예   2) 아니오.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("당신은 박격포를 맞고 사망하였습니다.");
+
+
+
+
+
+        }
+    }
+}
---
+++ study6/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Ruin_Skill_Damage = 0.0f;
+            float Card_Gage = 0.0f;
+            float Ultimate_Damage = 0.0f;
+            int Max_MP = 0;
+            int Regain_MP = 0;
+            int Nonfight_Regain_MP = 0;
+            float Speed = 0.0f;
+            float Speed_Riding = 0.0f;
+            float Speed_Carrying = 0.0f;
+            float Skill_Cool_Time = 0.0f;
+
+
+            Console.Write("스킬 피해량을 입력하세요.");
+            string Damage_input = Console.ReadLine();
+            Ruin_Skill_Damage = float.Parse(Damage_input);
+
+            Console.Write("카드 게이지 획득량을 입력하세요.");
+            string Card_input = Console.ReadLine();
+            Card_Gage = float.Parse(Card_input);
+
+            Console.Write("각성기 피해를 입력하세요.");
+            string Ultimate_input = Console.ReadLine();
+            Ultimate_Damage = float.Parse(Ultimate_input);
+
+            Console.Write("최대 마나를 입력하세요.");
+            string MaxMP_input = Console.ReadLine();
+            Max_MP = int.Parse(MaxMP_input);
+
+
+            Console.Write("전투중 마나 회복량을 입력하세요.");
+            string RegainMP_input = Console.ReadLine();
+            Regain_MP = int.Parse(RegainMP_input);
+
+            Console.Write("비전투중 마나 회복량을 입력하세요.");
+            string NonfightRegainMP_input = Console.ReadLine();
+            Nonfight_Regain_MP = int.Parse(NonfightRegainMP_input);
+
+
+
+
+
+
+            Console.WriteLine($"루인 스킬 피해 : {Ruin_Skill_Damage:F1}%");
+            Console.WriteLine($"카드 게이지 획득량 : {Card_Gage:F1}%");
+            Console.WriteLine($"각성기 피해 : {Ultimate_Damage:F1}%");
+            Console.WriteLine($"최대 마나 : {Max_MP:F1}%");
+            Console.WriteLine($"전투중 마나 회복량 : {Regain_MP:F1}%");
+            Console.WriteLine($"비전투중 마나 회복량 : {Regain_MP:F1}%");
+
+        }
+    }
+}
