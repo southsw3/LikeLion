eac84c04c14fe7388b0fb068bb6472738e5ae06f
southsw3 <southsw3@naver.com>
2025-02-21 18:02:47 +0900

    studyLine

---
+++ storyLine/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace storyLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loading.");
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(1000); //1000 1초
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("Loading..");
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000); //1000 1초
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("Loading...");
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(1000); //1000 1초
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("Loading.");
+            Console.WriteLine("□■■■□□□□□□");
+            Thread.Sleep(1000); //1000 1초
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("Loading..");
+            Console.WriteLine("□□■■■□□□□□");
+            Thread.Sleep(1000); //1000 1초
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("Loading...");
+            Console.WriteLine("□□□■■■□□□□");
+            Console.Clear();
+            Console.WriteLine("Loading.");
+            Console.WriteLine("□□□□■■■□□□");
+            Thread.Sleep(1000); //1000 1초
+            Console.Clear(); //콘솔 화면 지우기
+            Console.WriteLine("Loading..");
+            Console.WriteLine("□□□□□■■■□□");
+            Thread.Sleep(1000); //1000 1초
+            Console.Clear(); //콘솔 화면 지우기
+
+        }
+    }
+}
