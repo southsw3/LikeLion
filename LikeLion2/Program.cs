bdaa0e8f058a853a914a4cc4c6f476c66ba2b741
southsw3 <southsw3@naver.com>
2025-02-21 10:22:50 +0900

    study2

---
+++ LikeLion2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = 25;
+            
+            Console.WriteLine(age);
+
+
+        }
+    }
+}
