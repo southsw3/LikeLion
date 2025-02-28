0a5c35e0269fb4919bed2d9dd55c668633133fb4
southsw3 <southsw3@naver.com>
2025-02-28 15:34:45 +0900

    shooting game3

---
+++ study17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine($"Current Date and Time : {now}");
+
+            TimeSpan duration = new TimeSpan(1, 30, 0);
+            Console.WriteLine($"Duration : {duration}");
+        }
+    }
+}
---
+++ study18/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using study18;
+
+namespace study18
+{
+    //마린 클래스를 만드세요.
+    //이름,미네랄  = 50
+    //기본생성자 , 인자있는생성자 
+    //SCV 클래스를 만드세요.
+    //이름,미네랄  = 50
+    //기본생성자 , 인자있는생성자 
+    class Marin
+    {
+        public string Name;
+        public int Mineral;
+
+        public Marin()
+        {
+            Name = "마린";
+            Mineral = 50;
+        }
+
+        public Marin(string _name, int _mineral)
+        {
+            Name = _name;
+            Mineral = _mineral;
+        }
+
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"이름 : {Name}, 미네랄 : {Mineral}");
+        }
+    }
+
+    class SCV
+    {
+        public string Name;
+        public int Mineral;
+
+        public SCV()
+        {
+            Name = "SCV";
+            Mineral = 50;
+        }
+
+        public SCV(string _name, int _mineral)
+        {
+            Name = _name;
+            Mineral = _mineral;
+        }
+
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"이름 : {Name}, 미네랄 : {Mineral}");
+        }
+    }
+
+    //배럭클래스를 만드세요
+    //Barrack  150
+    //this 키워드를 이용해보자
+    //this 자기자신을 가르킨다.
+
+    class Barrack
+    {
+        public string Name;
+        public int Mineral;
+
+        public Barrack()
+        {
+            Name = "배럭";
+            Mineral = 150;
+        }
+
+        public Barrack(string Name, int Mineral)
+        {
+            this.Name = Name;
+            this.Mineral = Mineral;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"이름 : {Name} 미네랄 : {Mineral}");
+        }
+
+    }
+    //미네랄클래스를 만드시오
+    //Mineral 1500 기본으로 주네요
+    //7개가 시작부터 있습니다.
+    //클래스화 해봅시다.
+    class Mineral
+    {
+        public int MineralCount;
+
+        public Mineral()
+        {
+            MineralCount = 1500;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"현재 미네랄 갯수 : {MineralCount}");
+        }
+
+        class Game
+        {
+            public static int mineral;
+            public static int gas;
+            public static int charCount;
+
+            public static void ShowInfo()
+            {
+                Console.WriteLine($"미네랄 {mineral} 가스 {gas} 인구수 {charCount}");
+            }
+
+
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Game.mineral = 50;
+                Game.gas = 50;
+                Game.charCount = 4;
+                Game.ShowInfo();
+
+                Marin marin = new Marin("불꽃테란", 100);
+                SCV scv = new SCV("열받은SCV", 70);
+                Barrack barrack = new Barrack();
+                //클래스의 배열 7개 생성
+                Mineral[] mineral = new Mineral[7];
+
+                //각 배열에 new 객체화
+                for (int i = 0; i < mineral.Length; i++)
+                {
+                    mineral[i] = new Mineral();
+                    mineral[i].ShowInfo();
+                }
+
+
+
+
+
+
+                marin.ShowInfo();
+
+                scv.ShowInfo();
+                barrack.ShowInfo();
+
+
+            }
+        }
+    }
+}
+
